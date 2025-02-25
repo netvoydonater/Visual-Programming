b9e38a36914e2888b64fd860b4ed365ad64a9289
Подкорытова Александра Валерьевна <ip311s18@csc.sibsutis.ru>
2025-02-25 09:18:10 +0700

    Fix bugs in orderBy.js

--- dz2/orderBy.js
+++ dz2/orderBy.js
@@ -1,20 +1,19 @@
-function orderBy(arr, props) 
-{
+function orderBy(arr, props) {
     if (!Array.isArray(arr)) {
         throw new TypeError('First argument must be an array');
     }
-    
+
     arr.forEach(item => {
         if (typeof item !== 'object' || item === null) {
             throw new TypeError('All elements in array must be objects');
         }
         props.forEach(prop => {
             if (!(prop in item)) {
-                throw new Error(`Property '${prop}' is missing in one of the objects`);
+                throw new Error(`Property '${prop}' is missing in one of the objects`); 
             }
         });
     });
-    
+
     return [...arr].sort((a, b) => {
         for (let prop of props) {
             if (a[prop] > b[prop]) return 1;
