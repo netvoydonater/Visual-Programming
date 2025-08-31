2c9f0b9978df207fe0569f32e96d8dbd309f119c
sancha <162143735+netvoydonater@users.noreply.github.com>
2025-08-31 17:34:53 +0700

    добавлены файлы 11 лабы
    для запуска нужна готовая база данных, поэтому ориентируйтесь на код и собирайте БД сами. в директории содержатся две лабы: 10-ая и 11-ая

---
+++ dz10/dz11/dz11/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using dz11.Models;
+namespace dz11.Repositories
+{
+    public interface ICommentRepository
+    {
+        void createComment(int postId, string name, string email, string body);
+        void UpdateId(int id, int postId, string name, string email, string body);
+        void deleteById(int id);
+        Comment GetCommentById(int id);
+        IEnumerable<Comment> GetAllTables();
+    }
+}
